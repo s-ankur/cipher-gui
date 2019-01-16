9df6f5faabec1d78044ccbc86bcef9bcf31bd28c
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2019-01-16 19:46:10 +0530

    Added Options in GUI

--- main.py
+++ main.py
@@ -12,37 +12,61 @@ AVAILABLE_CIPHERS = ('atbash', 'caesar')
 class CipherGUI:
     ciphers = {name: __import__(name) for name in AVAILABLE_CIPHERS}
 
-    def __init__(self, current='atbash'):
-        self.current = current
-        self.create_gui()
+    def __init__(self, ):
+        self.create_textbox()
+        self.create_options()
+        tk.mainloop()
 
-    def create_gui(self):
-        self.window = tk.Tk()
-        self.window.configure(background='white')
-        self.window.title("Cipher Gui")
-        self.window.geometry("800x150")
+    def create_options(self):
+        self.options = tk.Toplevel()
+        tk.Label(self.options, text='Cipher: ').grid(column=0,row=0)
+        self.options.title("Options")
+        self.cipher_choice_text = tk.StringVar()
+        self.cipher_choice_text.set(AVAILABLE_CIPHERS[0])
+        self.cipher_choice = tk.OptionMenu(self.options, self.cipher_choice_text, *AVAILABLE_CIPHERS)
+        self.cipher_choice.grid(row=0, column=1)
+        tk.Button(self.options, text="Encrypt", command=self.on_encrypt).grid(row=1, column=0)
+        tk.Button(self.options, text="Decrypt", command=self.on_decrypt()).grid(row=1, column=1)
+        tk.Button(self.options, text="Hack", command=self.on_hack).grid(row=1, column=2)
 
+
+    def create_textbox(self):
+        self.textbox = tk.Tk()
+
+        self.textbox.configure(background='white')
+        self.textbox.title("Cipher Gui")
+        self.textbox.geometry("800x150")
+
+        self.key_text = tk.StringVar()
+        self.key_text.set("Key")
+        self.key = tk.Entry(self.textbox, font='Calibri 13', textvariable=self.key_text)
         self.source_text = tk.StringVar()
         self.source_text.set("Plaintext")
-        self.source = tk.Entry(self.window, font='Calibri 13', textvariable=self.source_text)
+        self.source = tk.Entry(self.textbox, font='Calibri 13', textvariable=self.source_text)
         self.target_text = tk.StringVar()
         self.target_text.set("Ciphertext")
+        self.target = tk.Entry(self.textbox, font='Calibri 13', textvariable=self.target_text)
 
-        self.target = tk.Entry(self.window, font='Calibri 13', textvariable=self.target_text)
-
+        self.key.pack(side=TOP, fill=BOTH, expand=0)
+        self.key.configure(background='white')
         self.source.pack(side=LEFT, fill=BOTH, expand=1)
-        self.source.bind('<Return>', self.on_enter_source)
-        self.target.pack(side=RIGHT, fill=BOTH, expand=1)
-        self.target.bind('<Return>', self.on_enter_target)
+        self.source.bind('<Return>', self.on_encrypt)
         self.source.configure(background='white')
+        self.target.pack(side=RIGHT, fill=BOTH, expand=1)
+        self.target.bind('<Return>', self.on_decrypt)
         self.target.configure(background='white')
-        self.window.mainloop()
 
-    def on_enter_source(self, _):
-        self.target_text.set(self.ciphers[self.current].encrypt(self.source_text.get()))
+    def get_cipher(self):
+        return self.ciphers[self.cipher_choice_text.get()]
 
-    def on_enter_target(self, _):
-        self.source_text.set(self.ciphers[self.current].decrypt(self.target_text.get()))
+    def on_encrypt(self, _='unused'):
+        self.target_text.set(self.get_cipher().encrypt(self.source_text.get(), self.key_text.get()))
+
+    def on_decrypt(self, _='unused'):
+        self.source_text.set(self.get_cipher().decrypt(self.target_text.get(), self.key_text.get()))
+
+    def on_hack(self, _='unused'):
+        self.source_text.set(self.get_cipher().hack(self.target_text.get(), self.key_text.get()))
 
 
 if __name__ == '__main__':
