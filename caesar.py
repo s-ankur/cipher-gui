2721901daae37b96c1863c1705ca1856ea1d1418
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2019-01-20 15:15:42 +0530

    Added crack for caesar

--- caesar.py
+++ caesar.py
@@ -7,6 +7,12 @@ by a letter some fixed number of positions down the alphabet. For example,
 with a left shift of 3, D would be replaced by A, E would become B, and so on.
 The method is named after Julius Caesar, who used it in his private correspondence.
 """
+from collections import Counter
+from functools import lru_cache
+import string
+
+FREQ_LETTERS = 'ETAOINSRHDLUCMFYWGPBVKXQJZ'
+
 
 
 def encrypt_letter(letter, key):
@@ -21,7 +27,39 @@ def encrypt(plaintext, key):
     key = ord(key.lower()[0]) - ord('a')
     return ''.join(list(map(lambda x: encrypt_letter(x, key), plaintext)))
 
-
 def decrypt(ciphertext, key):
     key = - (ord(key.lower()[0]) - ord('a'))
     return ''.join(list(map(lambda x: encrypt_letter(x, key), ciphertext)))
+
+def lcs(X , Y): 
+    # find the length of the strings 
+    m = len(X) 
+    n = len(Y) 
+  
+    L = [[None]*(n+1) for i in range(m+1)] 
+
+    for i in range(m+1): 
+        for j in range(n+1): 
+            if i == 0 or j == 0 : 
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]: 
+                L[i][j] = L[i-1][j-1]+1
+            else: 
+                L[i][j] = max(L[i-1][j] , L[i][j-1]) 
+    return L[m][n] 
+
+def crack(ciphertext):
+    ciphertext_orig=ciphertext
+    ciphertext = ciphertext.upper()
+    candidates = []
+    for candidate_key in string.ascii_lowercase:
+        print('trying',candidate_key)
+        candidate_plaintext = decrypt(ciphertext,candidate_key)
+        freq_cipher=''.join([letter for letter,count in Counter(candidate_plaintext).most_common() if letter.isalpha()])
+        print(freq_cipher)
+        candidates.append((lcs (freq_cipher,FREQ_LETTERS),candidate_key))
+    key=max(candidates)[1]
+    plaintext= decrypt(ciphertext_orig,key)
+    return plaintext,key
+    
+    
