b1eeaf82e5a990583880a825b8cf9f21a533296a
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2019-01-21 01:06:20 +0530

    Added hack for vignere

--- vigenere.py
+++ vigenere.py
@@ -6,8 +6,11 @@ series of interwoven Caesar ciphers, based on the letters of a keyword.
 It is a form of polyalphabetic substitution.
 """
 from itertools import cycle
+from collections import Counter
 import caesar
 
+Kp=0.067
+Kt=0.0385
 
 def encrypt(plaintext, key):
     key= map(lambda x: ord(x)-ord('a'),key.lower())
@@ -17,3 +20,20 @@ def encrypt(plaintext, key):
 def decrypt(ciphertext, key):
     key= map(lambda x: -(ord(x)-ord('a')),key.lower())
     return ''.join(list(map(lambda x:caesar.encrypt_letter(*x), zip(ciphertext,cycle(key)))))
+
+def crack(ciphertext):
+    ciphertext_freq = Counter(filter(str.isalpha,ciphertext.lower()))
+    N = sum(ciphertext_freq.values())
+    sigma = sum(map(lambda freq: freq*(freq-1),ciphertext_freq.values()))
+    Ko=sigma/(N*(N-1))
+    length = (Kp-Kt)/(Ko-Kt)
+    length = round(length)
+    key=[]
+    for i in range(length):
+        column=ciphertext[i::length]
+        text,key_letter=caesar.crack(column)
+        key.append(key_letter)
+    key=''.join(key)
+    plaintext= decrypt(ciphertext,key)
+    return plaintext , key
+        
